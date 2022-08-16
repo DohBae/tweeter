231c32582087d7bd6bc6c5c66a4b37952d5d2901
DohBae <bdoherty92@msn.com>
2022-08-16 21:54:13 +0000

    Rewrite code for count down characters and turn red when limit exceeded

--- public/styles/composer-char-counter.js
+++ public/styles/composer-char-counter.js
@@ -1,21 +1,18 @@
 $(document).ready(function() {
   
-  let maxCharacters = 140
-  
-  $(".counter").text(maxCharacters);
-  
-  $(".textarea").bind("keyup keydown", function () {
-    let characterCount = $(".counter");
-    let characters = $(this).val().length;
-  
-    if (characters > maxCharacters) {
-      characterCount.addClass("over");
+$("#tweet-text").on("input", function (event) {
+  const maxCharacters = 140  
+    let characterLength= event.target.value.length;
+    let charactersLeft = 140 - characterLength;
+    if (characterLength > maxCharacters) {
+      $("#characterCount").css("color", "red");
+      // $(this).siblings(".counter").val(maxCharacters)
     } else {
-      characterCount.removeClass("over");
+      // $(this).siblings(".counter")
+      $("#characterCount").css("color", "#545149");
     }
-  
-    characterCount.text(maxCharacters - characters)
-  });
+    $("#characterCount").text(charactersLeft)
+  })
   
 });
 
@@ -29,3 +26,20 @@ $(document).ready(function() {
 
 
 
+
+// let maxCharacters = 140
+
+// $(".counter").text(maxCharacters);
+
+// $(".textarea").bind("keyup keydown", function () {
+//   let characterCount = $(".counter");
+//   let characters = $(this).val().length;
+
+//   if (characters > maxCharacters) {
+//     characterCount.addClass("over");
+//   } else {
+//     characterCount.removeClass("over");
+//   }
+
+//   characterCount.text(maxCharacters - characters)
+// });
