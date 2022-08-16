b9ee3cff4280f445448119dae4fa749187d888b9
DohBae <bdoherty92@msn.com>
2022-08-16 21:55:25 +0000

    Clean up old comments

--- public/styles/composer-char-counter.js
+++ public/styles/composer-char-counter.js
@@ -6,40 +6,10 @@ $("#tweet-text").on("input", function (event) {
     let charactersLeft = 140 - characterLength;
     if (characterLength > maxCharacters) {
       $("#characterCount").css("color", "red");
-      // $(this).siblings(".counter").val(maxCharacters)
     } else {
-      // $(this).siblings(".counter")
       $("#characterCount").css("color", "#545149");
     }
     $("#characterCount").text(charactersLeft)
   })
   
 });
-
-
-
-
-
-
-
-
-
-
-
-
-// let maxCharacters = 140
-
-// $(".counter").text(maxCharacters);
-
-// $(".textarea").bind("keyup keydown", function () {
-//   let characterCount = $(".counter");
-//   let characters = $(this).val().length;
-
-//   if (characters > maxCharacters) {
-//     characterCount.addClass("over");
-//   } else {
-//     characterCount.removeClass("over");
-//   }
-
-//   characterCount.text(maxCharacters - characters)
-// });
