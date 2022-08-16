401cf3b1c5fefaff401026a6fb73dcf78fe6bd1d
DohBae <bdoherty92@msn.com>
2022-08-16 20:34:44 +0000

    Count down characters, turn red when limit exceeded

--- public/styles/composer-char-counter.js
+++ public/styles/composer-char-counter.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
-  console.log("hello")
+  
+  let maxCharacters = 140
+  
+  $(".counter").text(maxCharacters);
+  
+  $(".textarea").bind("keyup keydown", function () {
+    let characterCount = $(".counter");
+    let characters = $(this).val().length;
+  
+    if (characters > maxCharacters) {
+      characterCount.addClass("over");
+    } else {
+      characterCount.removeClass("over");
+    }
+  
+    characterCount.text(maxCharacters - characters)
+  });
+  
 });
+
+
+
+
+
+
+
+
+
+
+
